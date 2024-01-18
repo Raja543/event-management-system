f5b5353fb7c39b027a9ebf96661ad24410187f88
Raja Kumar <rajakumarmahto952@gmail.com>
2024-01-18 23:30:48 +0530

    adding navbar

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { HashLink } from "react-router-hash-link";
+
+const Navbar = () => {
+  return (
+    <div className="px-24 py-8 flex flex-row items-center justify-between">
+      <div className="font-bold text-4xl">
+        Event
+        <span className="text-[#7848F4]"> Hive</span>
+      </div>
+      <div className="flex flex-row items-center justify-center gap-4">
+        <HashLink smooth to="/login">
+          <button className="px-4 font-bold">Login</button>
+        </HashLink>
+        <HashLink smooth to="/signup">
+          <button className="px-6 py-3 rounded-md bg-[#7848F4] text-white font-bold ml-4">
+            Signup
+          </button>
+        </HashLink>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
