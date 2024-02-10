90840af1c3db07837b40db821c7f687ea2d7f896
Raja Kumar <rajakumarmahto952@gmail.com>
2024-02-11 02:25:52 +0530

    fixing path event controller

--- server/routes/EventRoute.js
+++ server/routes/EventRoute.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const {createEvent , updateEvent , deleteEvent} = require("./EventController");
+const {createEvent , updateEvent , deleteEvent} = require("../controllers/EventController");
 const { checkEventExists } = require('../middlewares/EventMiddleware');
 
 
